a2cb3f736f3319a1ef3333106a4e1e0373940211
AlanWehrliLC <wehrlialan1@gmail.com>
2022-01-16 23:31:22 -0300

    cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -12,6 +12,7 @@ import { router } from "./routes"
 import { AppError } from "@shared/errors/AppError"
 import swaggerFile from "../../../swagger.json"
 import upload from "@config/upload"
+import cors from "cors"
 
 createConnection()
 const app = express();
@@ -22,6 +23,8 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`))
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`))
+
+app.use(cors())
 
 app.use(router)
 
